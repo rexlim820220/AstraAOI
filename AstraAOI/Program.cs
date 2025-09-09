b813c4ca3f8e9d456f1b468efcba6b18af1f7f73
Pisces_Zhang(張洪瑞_Pegatron) <Pisces_Zhang@pegatroncorp.com>
2025-09-09 10:11:45 +0800

    Update AstraAOI project file and add initial Program.cs entry point

---
+++ AstraAOI/Program.cs
@@ -0,0 +1,22 @@
+﻿using Basler.Pylon;
+
+class Program
+{
+    static void Main()
+    {
+        Camera camera = new Camera();
+        camera.Open();
+        camera.Parameters[PLCameraInstance.MaxNumBuffer].SetValue(5);
+        camera.StreamGrabber.Start(1, GrabStrategy.OneByOne, GrabLoop.ProvidedByStreamGrabber);
+
+        IGrabResult result = camera.StreamGrabber.RetrieveResult(5000, TimeoutHandling.ThrowException);
+        if (result.GrabSucceeded)
+        {
+            Console.WriteLine($"Image Size: {result.Width} x {result.Height}");
+        }
+
+        result.Dispose();
+        camera.Close();
+    }
+}
+
